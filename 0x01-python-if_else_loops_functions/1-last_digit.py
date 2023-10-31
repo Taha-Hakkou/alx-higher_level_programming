1b5bc6c5566a43a0f2e1c7ad17642ce746815bcc
Taha-Hakkou <hakkoutaha16@gmail.com>
2023-10-31 22:58:38 +0100

    0x01-code errors correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number:d} is", end=" ")
-if number < 0:
-    number = -number
-last_digit = number % 10
-print(f"{last_digit:d} and is", end=" ")
+last_digit = number % 10 if number >= 0 else number % 10 - 10
+print(f"Last digit of {number:d} is {last_digit:d} and is", end=" ")
 if last_digit > 5:
     print("greater than 5")
 elif last_digit == 0:
--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    pow = 1
-    if b >= 0:
-        for i in range(b):
-            pow = pow * a
-    else:
-        for i in range(-b):
-            pow = pow / a
-    return (pow)
+    return (a ** b)
