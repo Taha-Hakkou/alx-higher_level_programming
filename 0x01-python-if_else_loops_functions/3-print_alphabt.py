1067067f4d6d8f5fa0677f64b14b716504a575bb
Taha-Hakkou <hakkoutaha16@gmail.com>
2023-10-31 22:19:21 +0100

    0x01-format string correction

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alpha in range(ord('a'),ord('z')+1):
     if chr(alpha) not in 'qe':
-        print(f"{chr(alpha)}",end="")
+        print("{}".format(chr(alpha)),end="")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for char in str:
         if ord(char) in range(ord('a'),ord('z')+1):
             char = chr(ord(char) + ord('A') - ord('a'))
-        print(f"{char}",end="")
+        print("{}".format(char),end="")
     print()
 
