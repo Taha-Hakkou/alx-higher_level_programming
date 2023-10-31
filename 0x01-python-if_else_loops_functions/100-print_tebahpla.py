15e3b85a1b3ce9f3ff1a7140fa27976216ff69ae
Taha-Hakkou <hakkoutaha16@gmail.com>
2023-10-31 23:28:37 +0100

    0x01-Pycodestyle 2nd revision

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a')-1, -1):
-    print("{}".format(chr(i) if i%2==0 else chr(i+ord('A')-ord('a'))), end="")
+i = 0
+for c in range(ord('z'), ord('a')-1, -1):
+    print("{}".format(chr(c + i)), end="")
+    i = ord('A') - ord('a') if i == 0 else 0
