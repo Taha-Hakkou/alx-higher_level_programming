247bfd2df197d6030c905b2c26985c1dea6642cc
Taha-Hakkou <hakkoutaha16@gmail.com>
2023-11-02 22:47:31 +0100

    0x02-Pycodestyle solving2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     """simple calculator"""
     from calculator_1 import add, sub, mul, div
-    
+
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
