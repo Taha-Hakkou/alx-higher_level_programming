0d2d0de67586413b6dfd24ee5b0c6cdd96b5f951
Taha-Hakkou <hakkoutaha16@gmail.com>
2023-11-06 11:41:09 +0100

    0x03-Tech interview efficiency tests approved

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,26 @@
 #include "lists.h"
 #include <stdio.h>
+
+/**
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node, *next, *prev;
+
+	prev = NULL;
+	for (node = *head; node; node = next)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+	}
+	*head = prev;
+	return (*head);
+}
+
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: double pointer to first node of linked list
@@ -8,23 +29,31 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *node1, *node2;
-	int length, i, j;
+	int length, i;
 
 	node1 = *head;
-	if (!node1)
-		return (1);
-	for (length = 1; node1->next; length++)
+	/* count length of linked list */
+	length = 0;
+	for (; node1; length++)
 		node1 = node1->next;
+	/* reverse second half of the linked list */
+	node1 = *head;
+	for (i = 0; i < length / 2 - 1; i++)
+		node1 = node1->next;
+	node2 = reverse_listint(&(node1->next));
+	/* check if palindrome */
+	node1 = *head;
 	for (i = 0; i < length / 2; i++)
 	{
-		node1 = *head;
-		node2 = *head;
-		for (j = 0; j < i; j++)
-			node1 = node1->next;
-		for (j = 0; j < length - i - 1; j++)
-			node2 = node2->next;
 		if (node1->n != node2->n)
 			return (0);
+		if (i < length / 2 - 1)
+		{
+			node1 = node1->next;
+			node2 = node2->next;
+		}
 	}
+	/* re-arrange the linked list */
+	reverse_listint(&(node1->next));
 	return (1);
 }
