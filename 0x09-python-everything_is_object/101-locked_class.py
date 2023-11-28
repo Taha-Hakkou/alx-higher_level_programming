4ec0563815c73070f319f787aed84006549f05d2
Taha-Hakkou <hakkoutaha16@gmail.com>
2023-11-28 22:02:33 +0100

    0x09 errors corrected2

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,13 +7,4 @@
 class LockedClass:
     """locked class"""
 
-    def __setattr__(self, name, value):
-        """prevents dynamic creation of new instance attributes,
-        except if the new instance attribute is called first_name"""
-        if name == 'first_name':
-            if type(value) != str:
-                raise TypeError("'first_name' attribute must be a string")
-            self.__dict__[name] = value
-        else:
-            m = f"'{self.__class__.__name__}' object has no attribute '{name}'"
-            raise AttributeError(m)
+    __slots__ = ["first_name"]
