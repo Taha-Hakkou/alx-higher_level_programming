7091d1286f0470c85304582583fbabc1f5aa4207
Taha-Hakkou <hakkoutaha16@gmail.com>
2024-01-21 10:25:25 +0100

    0x0D corrections

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- 9-full_creation.sql
 -- creates a table second_table in the database hbtn_0c_0 in your MySQL server
-CREATE TABLE second_table(
-id INT
-name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS second_table(
+id INT,
+name VARCHAR(256),
 score INT
 );
 -- add multiple rows
@@ -11,4 +11,3 @@ INSERT INTO second_table(id, name, score) VALUES
 (2, 'Alex', 3),
 (3, 'Bob', 14),
 (4, 'George', 8);
-
