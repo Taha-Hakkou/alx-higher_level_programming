06e970c9904d6b5545c8b35b47356f9cd388780b
Taha-Hakkou <hakkoutaha16@gmail.com>
2024-02-05 17:17:03 +0100

    sql queries mandatory corrections

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- 12-no_genre.sql
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT title, genre_id FROM tv_shows
-RIGHT JOIN tv_show_genres ON genre_id = tv_shows.id
-WHERE tv_shows.id IS NULL
+LEFT JOIN tv_show_genres ON show_id = tv_shows.id
+WHERE genre_id IS NULL
 ORDER BY title, genre_id;
--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,7 @@
 -- 8-cities_of_california_subquery.sql
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT * FROM cities
-WHERE state_id = states.id AND states.name = 'California'
+WHERE id IN (
+SELECT id FROM states
+WHERE name = 'California')
 ORDER BY cities.id;
