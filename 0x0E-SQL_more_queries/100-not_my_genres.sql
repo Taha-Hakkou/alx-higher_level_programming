64c56e9eb7fc9c45a99f3d05058f58f6ce59bd73
Taha-Hakkou <hakkoutaha16@gmail.com>
2024-02-05 17:38:30 +0100

    sql queries advanced corrections

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@
 SELECT name FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = genre_id
 INNER JOIN tv_shows ON tv_shows.id = show_id
-WHERE title NOT IN (
+WHERE name NOT IN (
 SELECT name FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = genre_id
 INNER JOIN tv_shows ON tv_shows.id = show_id
--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,6 +1,6 @@
 -- 101-not_a_comedy.sql
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
-SELECT title FROM tv_shows
+SELECT DISTINCT title FROM tv_shows
 LEFT JOIN tv_show_genres ON show_id = tv_shows.id
 LEFT JOIN tv_genres ON genre_id = tv_genres.id
 WHERE title NOT IN (
