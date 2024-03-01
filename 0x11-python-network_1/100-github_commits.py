fa7df4ec81615fefe5dd9d9598101b77a55cb5fc
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-03-01 23:02:59 +0100

    0x11 py_net1 corr2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,6 @@ if __name__ == '__main__':
     repository, owner = sys.argv[1:]
     url = f'https://api.github.com/repos/{owner}/{repository}/commits'
     response = requests.get(url)
-    for element in response.json()[:10]:
-        commit = element.get('commit')
-        print(f'{commit.get("tree").get("sha")}: \
-{commit.get("author").get("name")}')
+    for commit in response.json()[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
