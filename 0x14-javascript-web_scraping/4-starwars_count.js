cb29dfc38daa431fb4823a3767276de1c58924c2
Taha-Hakkou <taha.hakkou42@gmail.com>
2024-03-27 01:38:00 +0000

    0x14 js_web_scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,12 +2,15 @@
 // prints the number of movies where the character “Wedge Antilles” is present
 const request = require('request');
 const url = process.argv[2];
-//const id = 18;
 let data = '';
 request.get(url)
   .on('data', (chunk) => { data += chunk; })
   .on('end', () => {
-    const count = JSON.parse(data).results
-      .filter((film) => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')).length;
-    console.log(count);
+    const n = JSON.parse(data).results
+      .reduce((count, movie) => {
+        return movie.characters.find((character) => character.endsWith('/18/'))
+          ? count + 1
+          : count;
+      }, 0);
+    console.log(n);
   });
